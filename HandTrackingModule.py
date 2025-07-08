eb11005db796b05629becaf27f004bc415e5b076
Mihirkool <mih.kul.2005@gmail.com>
2025-07-08 20:50:17 +0530

    Hand tracking module raw

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,47 +2,54 @@ import cv2
 import mediapipe as mp
 import time
 
+class handDetector():
+    def __init__(self,mode=False, maxHands = 2, detectionCon=0.5, trackCon = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands  # formality before writing a module
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode,  # fixed argument names
+                                        max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
-mpHands = mp.solutions.hands #formality before writing a module
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+    def findHands(self, img, draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
 
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:  # for multiple hands
 
+                if draw:
+                    self.mpDraw.draw_landmarks(
+                        img,
+                        handLms,
+                        self.mpHands.HAND_CONNECTIONS,  # to draw connections and dots
+                        self.mpDraw.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=1),  # red dots
+                        self.mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2)  # green lines
+                    )
+        return img
 
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    #print(results.multi_hand_landmarks)
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:  #for multiple hands
-            for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
-                h,w,c = img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h) #convert co-ordinates into pxl
-                print(id,cx,cy) #print id of location of all 20 points
-                if id == 4:
-                    cv2.circle(img, (cx, cy), 15, (255,0,255, cv2.FILLED))
-
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)  #to draw connections and dots
-
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-
-    cv2.putText(img, str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN, 3,
-                (225,0,225),3)
-
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
+                # for id, lm in enumerate(handLms.landmark):
+                    # print(id,lm)
+                    # h, w, c = img.shape
+                    # cx, cy = int(lm.x * w), int(lm.y * h)  # convert co-ordinates into pxl
+                    # print(id, cx, cy)  # print id of location of all 20 points
+                    # if id == 4:
+                    #     cv2.circle(img, (cx, cy), 15, (255, 0, 255, cv2.FILLED))
 
 def main():
     pTime = 0
     cTime = 0
 
     cap = cv2.VideoCapture(0)
-
+    detector = handDetector()
     while True:
         success, img = cap.read()
+        img = detector.findHands(img)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
